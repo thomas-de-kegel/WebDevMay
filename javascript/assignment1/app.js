a81da7ab56ac2202cba324b6bd330f95ed56260d
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-09 11:06:35 +0200

    assignment

---
+++ javascript/assignment1/app.js
@@ -0,0 +1,8 @@
+let temperature;
+temperature = 11;
+
+if (temperature >= 20) {
+    document.getElementById("scarf").innerText = "You do not need a scarf today!";
+} else {
+    document.getElementById("scarf").innerText = "You need a scarf today!";
+}
