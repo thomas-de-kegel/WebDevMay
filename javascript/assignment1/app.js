5a68ce7b24d3d0226a8be40e3024d054b18b2cf3
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-09 13:11:59 +0200

    Clean up in JS

--- javascript/assignment1/app.js
+++ javascript/assignment1/app.js
@@ -1,6 +1,4 @@
-
 let temperature = Math.floor(Math.random() * (31));
-
 
 // if (temperature > 19) {
 //     document.getElementById("scarf").innerHTML = "You do not need a scarf today!";
@@ -8,7 +6,8 @@ let temperature = Math.floor(Math.random() * (31));
 //     document.getElementById("scarf").innerHTML = "You need a scarf today!";
 // }
 
+//Cold will display either "You do need a scarf" or "You don't need a scarf", this will then be added in the Result Id in the HTML doc, as well as the value of temperature.
 let cold = (temperature <= 20)? "You do need a scarf!" : "You don't need a scarf!";
-document.getElementById("scarf").innerHTML = cold;
-
+document.getElementById("result").innerHTML = cold;
 document.getElementById("tempDisplay").innerHTML = temperature;
+
