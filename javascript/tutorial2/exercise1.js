aaa3dda4c90a1589126fb0541065c5a2ef6fecfa
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-10 11:54:48 +0200

    exercise1 first part done

---
+++ javascript/tutorial2/exercise1.js
@@ -0,0 +1,18 @@
+//BMI calc
+//with normal function
+function bmiCalc(weight,height){
+    return weight / (height * height);
+}
+
+console.log(bmiCalc(80,1.78));
+
+//with arrow function
+let bmiCalc2 = (weight,height) => weight / (height * height);
+
+console.log(bmiCalc2(80,1.78));
+
+//Netto calc
+//with normal function
+
+//with arrow function
+
--- javascript/tutorial2/functions.js
+++ javascript/tutorial2/functions.js
@@ -7,6 +7,7 @@ arrow function
 
 */
 
+/*
 function getSum(){
     //here function scope
 
@@ -15,5 +16,43 @@ function getSum(){
     return 0;
 }
 
-getSum();
-console.log(getSum());
+getSum(); */
+
+//console.log(getSum());
+
+function getSumOfNumbers(n1,n2){
+    return n1+n2; //the function returns the first value plus the second value
+} 
+
+console.log(getSumOfNumbers(4,5));
+// getSumOfNumbers({id:1},null);
+
+var Student ={
+    name:"Joe",
+    age:25,
+    isStudent:true,
+    isTeacher:false,
+    scores:[11,22,33,44,55,66,77,88]
+}
+
+function verifyStudent(studentParam){
+    console.log(studentParam); //function scope
+    let isStudent = studentParam.isStudent;
+    console.log(isStudent);
+    return isStudent? `NAME:${studentParam.name}\nAGE:${studentParam.age}\nSCORES:${studentParam.scores.toString()}` : "They are not a student."
+}
+
+//arrow function
+const verifyStudent2 = studentParam => {
+    let isStudent= studentParam.isStudent;
+    console.log("Arrow Function");
+    return isStudent? `NAME:${studentParam.name}\nAGE:${studentParam.age}\nSCORES:${studentParam.scores.toString()}`: "They're not a student."
+}
+
+const getSumOfNumbers = (n1,n2) => n1+n2;
+const testArrowFunction = ()=>console.log("Hello arrow function")
+
+// console.log(verifyStudent(Student));
+
+console.log(getSumOfNumbers(4,5));
+testArrowFunction();
