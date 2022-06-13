36de1fe38ee0b3dba4a0b13cb2a62fff1fb3884e
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-13 09:14:12 +0200

    objects

--- javascript/tutorial2/exercise1.js
+++ javascript/tutorial2/exercise1.js
@@ -13,6 +13,12 @@ console.log(bmiCalc2(80,1.78));
 
 //Netto calc
 //with normal function
+function nettoCalc(salary,tax){
+    let taxAmount = (salary*tax)/100;
+    let getNetto = salary - taxAmount;
+    return getNetto;
+}
 
+console.log(nettoCalc(1900,21));
 //with arrow function
 
