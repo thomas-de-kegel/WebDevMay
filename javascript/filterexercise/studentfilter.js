ecc5f8f4f46f951e161d3efa2bc521e64b82b457
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-15 19:05:31 +0200

    solved

--- javascript/filterexercise/studentfilter.js
+++ javascript/filterexercise/studentfilter.js
@@ -1,35 +1,33 @@
 var _students = [
     {
-        id:1,
-        name:'avarel',
-        course:'javascript',
-        score:45
+        id: 1,
+        name: 'avarel',
+        course: 'javascript',
+        score: 45
     },
     {
-        id:1,
-        name:'mike',
-        course:'java',
-        score:55
+        id: 1,
+        name: 'mike',
+        course: 'java',
+        score: 55
     },
     {
-        id:1,
-        name:'george',
-        course:'javascript',
-        score:66
+        id: 1,
+        name: 'george',
+        course: 'javascript',
+        score: 66
     },
     {
-        id:1,
-        name:'joe',
-        course:'python',
-        score:70
+        id: 1,
+        name: 'joe',
+        course: 'python',
+        score: 70
     }
 ]
 
 // whoPassedExam?
-function whoPassedExam(param){
-    newAray= param.filter(function (param){
-        return param.score >= 50;
-    })
+function whoPassedExam(param) {
+    return param.filter(value => value.score >= 50);
 
 };
 
