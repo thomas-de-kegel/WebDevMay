dea976b3f6cef59c5b31cdd7914be1ec312db41f
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-20 09:01:11 +0200

    updated

---
+++ javascript/self study/ArrayExperiment.js
@@ -0,0 +1,5 @@
+//Practice hungarian notation:
+// write in lowerCamelCase, with the lower letter indicating the type of variable it's supposed to be (oMember for an object called member, sName for a string, bPassed for a boolean etc)
+
+// For comment blocks use a //* and // /* at the end, as removing the first slash will toggle the visibility of the entire block
+
