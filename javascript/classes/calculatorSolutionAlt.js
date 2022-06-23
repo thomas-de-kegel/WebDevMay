cdf5e64d48a836b7f4e41fc5d5d87ce7f7081880
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-23 13:33:34 +0200

    inheritance started

--- javascript/classes/calculatorSolutionAlt.js
+++ javascript/classes/calculatorSolutionAlt.js
@@ -1,14 +1,14 @@
 class Calc {
-  constructor(number1, number2, operator) {
+  constructor(number1, number2) {
     this.result = 0;
     this.number1 = number1;
     this.number2 = number2;
-    this.operator = operator;
   }
 
   //get result
-  getResult() {
-    switch (this.operator) {
+  getResult(operator) {
+    this.operator = operator;
+    switch (operator) {
       case "+":
         this.result = this.number1 + this.number2;
         break;
@@ -35,18 +35,25 @@ class Calc {
 }
 
 console.clear();
-let app = new Calc(20, 10, "+"); //20 + 10 = 30
-app.getResult();
+// let app = new Calc(20, 10, "+"); //20 + 10 = 30
+// app.getResult();
+// app.toString();
+
+// let app2 = new Calc(25, 5, "*");
+// app2.getResult();
+// app2.toString();
+
+// let app3= new Calc(30,15,'/');
+// app3.getResult();
+// app3.toString();
+
+// let app4= new Calc(35,17,'-');
+// app4.getResult();
+// app4.toString();
+
+let app = new Calc(21, 5);
+app.getResult("-");
+app.getResult("+");
+app.getResult("/");
+app.getResult("*");
 app.toString();
-
-let app2 = new Calc(25, 5, "*");
-app2.getResult();
-app2.toString();
-
-let app3= new Calc(30,15,'/');
-app3.getResult();
-app3.toString();
-
-let app4= new Calc(35,17,'-');
-app4.getResult();
-app4.toString();
