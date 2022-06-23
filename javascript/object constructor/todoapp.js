593baddfd0a7ae1bf30e2be47e00ca38966f46df
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-23 08:50:49 +0200

    todo done

--- javascript/object constructor/todoapp.js
+++ javascript/object constructor/todoapp.js
@@ -1,25 +1,25 @@
 //TodoApplication.js
 
-function TodoApplication(){
-    this.todos=[];
+function TodoApplication() {
+  this.todos = [];
 }
 
-TodoApplication.prototype.addTodo= function(todo){
-    this.todos.push(todo);
-}
+TodoApplication.prototype.addTodo = function (todo) {
+  this.todos.push(todo);
+};
 
-TodoApplication.prototype.showTasks= function(){
-    console.log(this.todos)};
+TodoApplication.prototype.showTasks = function () {
+  console.log(this.todos);
+};
 
-TodoApplication.prototype.deleteTodo= function(value){
-    
-    let index= this.todos.indexOf(value);
-    console.log(index);    
-    if(index > -1){
-        console.log("Todo found: ", index, this.todos[index])
-        this.todos.splice(index,1);
-    }
-}
+TodoApplication.prototype.deleteTodo = function (value) {
+  let index = this.todos.indexOf(value);
+  console.log(index);
+  if (index > -1) {
+    console.log("Todo found: ", index, this.todos[index]);
+    this.todos.splice(index, 1);
+  }
+};
 
 let app = new TodoApplication();
 //app.showTasks(); //putting showTasks here will print your tasks before you've added them.
