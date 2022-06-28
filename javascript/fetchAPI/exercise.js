4287680c796b9d3017e239ba9b314a691d200f34
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-06-28 11:35:21 +0200

    exercise.html

--- javascript/fetchAPI/exercise.js
+++ javascript/fetchAPI/exercise.js
@@ -1,22 +1,21 @@
 //https://jsonplaceholder.typicode.com/users extract and sort user data
 //extract id, name, username, email
-import fetch from "node-fetch";
 
-
-const users = [
-
-]
+const users = [];
 
 //step 1: get all original data
-let getUsers = new Promise((resolve,reject)=>{
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res=>res.json())
-    .then(data=>{
-        resolve(data)
+let getUsers = new Promise((resolve, reject) => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => res.json())
+    .then((data) => {
+      resolve(data);
     })
-    .catch(err=>reject(err))
-})
+    .catch((err) => reject(err));
+});
 //step 2: extract specific properties from single user
+
 //step 3: users.push();
+
 //step 4: users
+
 //print users in a table
