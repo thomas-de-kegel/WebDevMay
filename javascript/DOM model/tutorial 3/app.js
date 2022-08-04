61228735a26585eb7915f46f684752335b0d824a
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-08-04 08:51:50 +0200

    tutorial 3

--- javascript/DOM model/tutorial 3/app.js
+++ javascript/DOM model/tutorial 3/app.js
@@ -1,17 +1,65 @@
-//window object
+// find elements
+const userInput = document.querySelector(".userinput");
+const saveBtn = document.querySelector(".savebtn");
+const getBtn = document.querySelector(".getbtn");
 
-//local storage
-const userInput = document.querySelector(".userinput"); //queryselector requires a . before the name in order to select a class or # in order to select an id
-const saveButton = document.querySelector(".savebutton");
-const getUsername = document.querySelector(".getusername");
+// let user = prompt('Enter your username');
+let user;
+// setTimeout(()=>{
+//     user = prompt('Enter your username');
+// },3000)
 
+// setInterval(()=>{
+//     user = prompt('Enter your username');
+//     if(JSON.parse(localStorage.getItem('students')) !==null){
+//         let getStorageData = JSON.parse(localStorage.getItem('students')) // temp localstorage data
+//         getStorageData.push(user) // add new data to temp array
+//         localStorage.setItem('students',JSON.stringify(getStorageData)) // re-write array data into localstorage
+//     }else{
+//         let students = [] // create new array
+//         students.push(user) // add new data to array
+//         localStorage.setItem('students',JSON.stringify(students)) // re-write array data into localstorage
+//     }
+// },3000)
 
-saveButton.addEventListener("click", () => {
-    console.log(`Hello there, ${userInput.value}`);
-    localStorage.setItem(`username: ${userInput.value}`);
-})
+document.addEventListener("contextmenu", (event) => {});
+document.addEventListener("selectstart", (event) => {
+  console.log("stop it!");
+});
 
-getUsername.addEventListener("click", () =>{
-    let username = localStorage.getItem("username");
-    console.log(`Username is ${username}`);
-})
+// let x = setInterval(()=>{
+//     alert('lol')
+// },Infinity)
+
+saveBtn.addEventListener("click", () => {
+  if (JSON.parse(localStorage.getItem("students")) !== null) {
+    let getStorageData = JSON.parse(localStorage.getItem("students")); // temp localstorage data
+    getStorageData.push(userInput.value); // add new data to temp array
+    localStorage.setItem("students", JSON.stringify(getStorageData)); // re-write array data into localstorage
+  } else {
+    let students = []; // create new array
+    students.push(userInput.value); // add new data to array
+    localStorage.setItem("students", JSON.stringify(students)); // re-write array data into localstorage
+  }
+});
+
+getBtn.addEventListener("click", () => {
+  // let username = localStorage.getItem('username')
+  // console.log(`Username is ${username}`)
+  let studentNames = JSON.parse(localStorage.getItem("students"));
+  console.log(studentNames);
+  console.log(user);
+  window.open("https://intecbrussel.be/", "_blank", "width=1000,height=1000");
+});
+
+// document.addEventListener('keyup',(e)=>{
+//     // let logs = localStorage.getItem('logs')
+//     // logs += `${e.key} `
+//     // localStorage.setItem('logs',logs)
+//     // console.log(localStorage.getItem('logs'))
+// })
+
+window.resizeTo(500, 500);
+
+var options = ["a", "b", "c", "d"];
+console.log(options[options.length - 1]);
