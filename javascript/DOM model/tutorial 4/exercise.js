f2f90bde8dd1504d4e7d0dca8ff6918d97b59424
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-08-04 11:06:23 +0200

    table done

--- javascript/DOM model/tutorial 4/exercise.js
+++ javascript/DOM model/tutorial 4/exercise.js
@@ -1,32 +1,92 @@
-// element generator
-const g = t=> document.createElement(t);
+// Element Generator
+const g = t => document.createElement(t);
 
-const randomImage= g('img')
-randomImage.src = 'https://picsum.photos/200'
-randomImage.className="img-fluid"
-randomImage.setAttribute("alt", "Random Image")
+// const randomImage = g('img')
+// randomImage.src = 'https://picsum.photos/200'
+// randomImage.className="img-fluid"
+// randomImage.setAttribute('alt','Random Image')
 
-const container = g('div')
-container.className='container';
-const row = g('div')
-row.className= "row justify-content-center align-items-center text-light"
-//collumn 1
-const col1 = g('div')
-const col1Text = document.createTextNode('col 1')
-col1.appendChild(col1Text)
-col1.className = "col-md-4 bg-primary p-5"
-//collumn 2
-const col2 = g('div')
-col2.appendChild(randomImage)
-col2.className = "col-md-4 bg-warning p-5"
-//collumn 3
-const col3 = g('div')
-const col3Text = document.createTextNode('col 3')
-col3.appendChild(col3Text)
-col3.className = "col-md-4 bg-danger p-5"
+// const container = g('div')
+// container.className="container";
+// const row = g('div')
+// row.className="row justify-content-center align-items-center text-light";
+// // column-1
+// const col1 = g('div')
+// const col1Text = document.createTextNode('Col 1')
+// col1.appendChild(col1Text)
+// col1.className="col-md-4 bg-primary p-5"
+// // column-2
+// const col2 = g('div')
+// col2.className="col-md-4 bg-warning p-5"
+// col2.appendChild(randomImage)
+// // column-3
+// const col3 = g('div')
+// col3.className="col-md-4 bg-danger p-5"
 
-row.append(col1)
-row.append(col2)
-row.append(col3)
-container.append(row) //we make all 3 collumns children of row, and make row a child of container
-document.body.append(container)
+// row.append(col1)
+// row.append(col2)
+// row.append(col3)
+// container.append(row)
+// document.body.append(container)
+
+// exercise-2 
+
+/*
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+*/
+
+const table = g('table')
+table.className="table"
+
+const thead = g('thead')
+const trOfThead = g('tr')
+
+
+const thIndex = g('th')
+thIndex.innerText="#"
+thIndex.setAttribute('scope','col')
+
+// trOfThead.append(thIndex) // appened th into tr
+
+const thFirst = g('th')
+thFirst.innerText="First"
+thFirst.setAttribute('scope','col')
+
+const thLast = g('th')
+thLast.setAttribute('scope','col')
+
+const thHandle = g('th')
+thHandle.setAttribute('scope','col')
+
+trOfThead.append(thIndex,thFirst,thLast,thHandle)
+thead.append(trOfThead)
+table.append(thead)
+console.log(table, thead)
