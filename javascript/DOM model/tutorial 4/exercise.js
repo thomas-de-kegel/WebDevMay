7dc114754e8edd1a1c6ef9de6a4021c29f8ac909
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-08-08 10:07:10 +0200

    morning lesson

--- javascript/DOM model/tutorial 4/exercise.js
+++ javascript/DOM model/tutorial 4/exercise.js
@@ -1,5 +1,5 @@
 // Element Generator
-const g = t => document.createElement(t);
+const g = (t) => document.createElement(t);
 
 // const randomImage = g('img')
 // randomImage.src = 'https://picsum.photos/200'
@@ -29,7 +29,7 @@ const g = t => document.createElement(t);
 // container.append(row)
 // document.body.append(container)
 
-// exercise-2 
+// exercise-2
 
 /*
 <table class="table">
@@ -63,51 +63,160 @@ const g = t => document.createElement(t);
 </table>
 */
 
-const table = g('table')
-table.className="table"
+// const table = g('table')
+// table.className = "table"
 
-const thead = g('thead')
-const trOfThead = g('tr')
+// const thead = g('thead')
+// const trOfThead = g('tr')
 
+// const thIndex = g('th')
+// thIndex.innerText = "#"
+// thIndex.setAttribute('scope', 'col')
 
-const thIndex = g('th')
-thIndex.innerText="#"
-thIndex.setAttribute('scope','col')
+// // trOfThead.append(thIndex) // appened th into tr
 
-// trOfThead.append(thIndex) // appened th into tr
+// const thFirst = g('th')
+// thFirst.innerText = "First"
+// thFirst.setAttribute('scope', 'col')
 
-const thFirst = g('th')
-thFirst.innerText="First"
-thFirst.setAttribute('scope','col')
+// const thLast = g('th')
+// thLast.setAttribute('scope', 'col')
 
-const thLast = g('th')
-thLast.setAttribute('scope','col')
+// const thHandle = g('th')
+// thHandle.setAttribute('scope', 'col')
 
-const thHandle = g('th')
-thHandle.setAttribute('scope','col')
+// trOfThead.append(thIndex, thFirst, thLast, thHandle)
+// thead.append(trOfThead)
+// table.append(thead)
 
-trOfThead.append(thIndex,thFirst,thLast,thHandle)
-thead.append(trOfThead)
-table.append(thead)
+// const tbody = g('tbody') // tbody
 
-const tbody = g('tbody') //tbody
+// const trOfTbody = g('tr') // tr of tbody
 
-const trOfTbody = g('tr') //child of tbody
+// const thOfTbody = g('th') // child of tr of tbody
+// thOfTbody.scope = 'row'
+// thOfTbody.innerText = '1'
 
-const thOfTbody = g('th') //child of tr of tbody
-thOfTbody.scope = 'row'
-thOfTbody.innerText= '1'
+// const tdFirst = g('td') // child of tr of tbody
+// tdFirst.innerText = "joe"
 
-const tdFirst = g('td') //child of tr of tbody
-tdFirst.innerText="joe"
+// const tdLast = g('td') // child of tr of tbody
+// tdLast.innerText = "dalton"
 
+// const tdHandle = g('td') // child of tr of tbody
+// tdHandle.innerText = "@joe"
+// trOfTbody.append(thOfTbody, tdFirst, tdLast, tdHandle)
+// tbody.append(trOfTbody)
+// table.append(tbody)
+// document.body.append(table)
 
-const tdLast = g('td') //child of tr of tbody
-tdLast.innerText="Dalton"
+/*
+<div class="card" style="width: 18rem;">
+<img src="https://picsum.photos/200" class="card-img-top" alt="...">
+ <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+</div> 
+    */
 
+// // Card Container
+// const CardContainer = g('div')
+// CardContainer.setAttribute('style','width: 18rem;')
 
-const tdHandle = g('td') //child of tr of tbody
-tdHandle.innerText="@Joe"
-trOfTbody.append(thOfTbody, tdFirst, tdLast, tdHandle)
-tbody.append(trOfTbody)
-console.log(table, thead)
+// // Card Image
+// const CardImage = g('img')
+// CardImage.setAttribute('src','https://picsum.photos/200')
+// //CardImage.classList.add('card-img-top')
+// CardImage.className="card-img-top"
+// CardImage.setAttribute('alt','Random')
+
+// const CardBody = g('div')
+// CardBody.classList.add('card-body')
+
+// const h5=g('h5')
+// h5.classList.add('card-title')
+// const CardTitleText = document.createTextNode('Card title')
+// h5.append(CardTitleText)
+
+// const desc = g('p')
+// desc.classList.add('card-text')
+// const CardText = document.createTextNode('Some quick example text to build on the card title and make up the bulk of the card\'s content.')
+// desc.append(CardText)
+
+// const CardBtn = g('a')
+// CardBtn.classList.add('btn')
+// CardBtn.classList.add('btn-primary')
+// CardBtn.innerText="Card Button"
+// CardBody.append(h5)
+// CardBody.append(desc)
+// CardBody.append(CardBtn)
+// CardContainer.append(CardImage)
+// CardContainer.append(CardBody)
+
+// const cloneOfCardContainer = CardContainer.cloneNode(true)
+// cloneOfCardContainer.children[1].children[0].innerText="New Card Title"
+
+// // change button style of clone one
+// cloneOfCardContainer.children[1].lastChild.classList.remove('btn-primary')
+// cloneOfCardContainer.children[1].lastChild.classList.add('btn-danger')
+// document.body.append(CardContainer)
+// document.body.append(cloneOfCardContainer)
+
+function MakeCardComponent(image, cardTitle, description, btnColor, btnText) {
+  // Card Container
+  const CardContainer = g("div");
+  CardContainer.setAttribute("style", "width: 18rem;");
+
+  // Card Image
+  const CardImage = g("img");
+  CardImage.setAttribute("src", `${image}`);
+  //CardImage.classList.add('card-img-top')
+  CardImage.className = "card-img-top";
+  CardImage.setAttribute("alt", "Random");
+
+  const CardBody = g("div");
+  CardBody.classList.add("card-body");
+
+  const h5 = g("h5");
+  h5.classList.add("card-title");
+  const CardTitleText = document.createTextNode(cardTitle);
+  h5.append(CardTitleText);
+
+  const desc = g("p");
+  desc.classList.add("card-text");
+  const CardText = document.createTextNode(description);
+  desc.append(CardText);
+
+  const CardBtn = g("a");
+  CardBtn.classList.add("btn");
+  CardBtn.classList.add(btnColor != undefined ? btnColor : "btn-primary");
+  CardBtn.innerText = btnText;
+  CardBody.append(h5);
+  CardBody.append(desc);
+  CardBody.append(CardBtn);
+  CardContainer.append(CardImage);
+  CardContainer.append(CardBody);
+  console.log(btnColor);
+  return document.body.append(CardContainer);
+}
+
+// https://nl.wikipedia.org/w/api.php
+MakeCardComponent(
+  "https://evolutionsports.nl/wp-content/uploads/2020/10/Martial-Art-stijlen-die-als-inspiraatsie-diende-voor-Avatar-The-Last-Airbender.jpeg",
+  "This is a test",
+  "lorem ipsum lorem ipsum",
+  "btn-success",
+  "Click me"
+);
+
+MakeCardComponent(
+  "https://evolutionsports.nl/wp-content/uploads/2020/10/Martial-Art-stijlen-die-als-inspiraatsie-diende-voor-Avatar-The-Last-Airbender.jpeg",
+  "Tasdasdasdasd",
+  "lorem ipsum lorem ipsum"
+);
+
+document.querySelector(".RemoveBtn").addEventListener("click", () => {
+  document.body.removeChild(document.body.children[2]);
+});
