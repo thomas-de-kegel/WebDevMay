3cc86a9ea14cb52d5afb4f993d591243b331eddc
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-08-16 13:34:33 +0200

    clean up

--- reactjs/my-app/src/childcomponent.js
+++ reactjs/my-app/src/childcomponent.js
@@ -1,3 +1,5 @@
+import socialmedia from "./component/socialmedia";
+
 /*
 function AnotherComponent(){
     return(
@@ -12,9 +14,27 @@ export default AnotherComponent;
 //*/
 
 export default function ChildComponent(){
+    //declare a variable
+    const companyName= "Google";
+    const someResult = 5*7;
+    function getSumOfNumber(a,b){
+        return a+b;
+    }
+    const printStudentName = name => {
+        return name;
+    }
+    const numbers= [1,2,3,4,5,6,7,8,9,10];
+
     return(
         <div>
-            <h1>Hello Hello</h1>
+            <h1>Hello Hello hello!</h1>
+            <h2>Heeeeeeellooo</h2>
+            {companyName}
+            <h1>{someResult}</h1>
+            <h2>{getSumOfNumber(2,5)}</h2>
+            <h3>{printStudentName("Jim")}</h3>
+            {console.log(companyName)}
+            {}
         </div>
     )
 }
---
+++ reactjs/my-app/src/component/socialmedia.js
@@ -0,0 +1,3 @@
+module.exports = "SocialIcons"
+
+const users = ["tim,tom,todd,tony,travis,TJ,bill"]
