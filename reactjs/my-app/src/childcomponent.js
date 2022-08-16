220739ef6d9f4609010b9f214d17c8eb97c577a7
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-08-16 12:04:52 +0200

    App created

---
+++ reactjs/my-app/src/childcomponent.js
@@ -0,0 +1,20 @@
+/*
+function AnotherComponent(){
+    return(
+        <div>
+            <h1>Hello Friends!</h1>
+        </div>
+    )
+}
+
+export default AnotherComponent;
+
+//*/
+
+export default function ChildComponent(){
+    return(
+        <div>
+            <h1>Hello Hello</h1>
+        </div>
+    )
+}
