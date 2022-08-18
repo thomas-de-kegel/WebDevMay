244b2540d34739cc6baff25f26adc263b4050c7a
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-08-18 15:10:50 +0200

    todo list updated and bug fix

--- reactjs/tutorial2/src/TodoList.js
+++ reactjs/tutorial2/src/TodoList.js
@@ -10,8 +10,21 @@ function TodoList() {
 
     }
 
+    function create_UUID(){ //We create a unique UUID so we don't have to use the index number as the key
+        var dt = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+        }
+
     const handleClick = (e) => {
         console.log("clickety clack =>", todo)
+        // let temp = todos; //other way to do this, but takes more lines (BUGGED)
+        // temp.push(todo);
+        // setTodos(temp);
         setTodos([...todos,todo]) //we add the latest todo to our existing todos list. Faster than converting it so we can use .push
     }
   return (
@@ -26,7 +39,16 @@ function TodoList() {
             aria-describedby="basic-addon1"
           />
           <Button onClick={handleClick}>Add</Button>
+          {console.log(todos)}
         </InputGroup>
+            <hr/>
+            <ul>
+                {
+                    todos.map(todo =>(
+                        <li key={create_UUID}> {todo}</li>
+                    ))
+                }
+            </ul>
       </Col>
     </Row>
   );
