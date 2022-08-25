0ba52ca1cc547b0c0273b619e2775596564f812b
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-08-25 11:16:16 +0200

    Navbar added

--- reactjs/tutorial4/src/Layout/Header.js
+++ reactjs/tutorial4/src/Layout/Header.js
@@ -1,18 +1,48 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Router, Routes, Link, Route } from "react-router-dom";
-
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import {AiFillTwitterSquare} from 'react-icons/fa'
 
 function Header() {
   return (
-    <div>
-      <nav>
-          <ul>
-            <li><Link to={"/"}>Homepage</Link></li> {/*Compared to using an <a> tag with a href, Link, being a react tag, doesn't reload the whole page when we click the link*/}
-            <li><Link to={"/Contact"}>Contact Page</Link></li>
-          </ul>
-        </nav>
-    </div>
-  )
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Blueberry</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link to={"/"} className="nav-link text-decoration-none">
+              Homepage
+            </Link>
+            <Link to={"/Contact"} className="nav-link text-decoration-none">
+              Contact
+            </Link>
+            <NavDropdown title="Categories" id="collasible-nav-dropdown">
+              <Link
+                to={"/Movies/Adventure"}
+                className="dropdown-item text-decoration-none"
+              >
+                Adventure
+              </Link>{" "}
+              {/*Links are just an example*/}
+              <Link
+                to={"/Movies/Horror"}
+                className="dropdown-item text-decoration-none"
+              >
+                Horror
+              </Link>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
-export default Header
+export default Header;
