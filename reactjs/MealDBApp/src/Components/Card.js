423e9324454e8fd75b8ea46f22f8ca4dff09491f
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-09-05 15:45:05 +0200

    Proper cards, search bar

--- reactjs/MealDBApp/src/Components/Card.js
+++ reactjs/MealDBApp/src/Components/Card.js
@@ -3,14 +3,14 @@ import Card from 'react-bootstrap/Card';
 
 function MealCard() {
   return (
-    <Card style={{ width: '16rem', margin: '0.5rem' }}>
-      <Card.Img variant="top" style={{height: "auto",}} src="https://www.themealdb.com/images/media/meals/xb97a81583266727.jpg" />
+    <Card className="meal-card" style={{ width: '16rem', margin: '0.5rem' }}>
+      <Card.Img variant="top" className="mt-3 meal-card-image" style={{height: "auto"}} src="https://www.themealdb.com/images/media/meals/xb97a81583266727.jpg" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
           ...
         </Card.Text>
-        <Button className="card-button">Check the recipe&#62;&#62;</Button>
+        <Button className="card-button" variant="warning">Check the recipe&#62;&#62;</Button>
       </Card.Body>
     </Card>
   );
--- reactjs/MealDBApp/src/Layout/Body.js
+++ reactjs/MealDBApp/src/Layout/Body.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Row, Col, ListGroup } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  ListGroup,
+  CardGroup,
+  Form,
+  FloatingLabel,
+} from "react-bootstrap";
 import Card from "../Components/Card";
 import CarouselComponent from "../Components/CarouselComponent";
 
@@ -11,23 +19,38 @@ function Body() {
         <Row>
           <Col md={2}>
             <ListGroup className="m-2 category-list">
-              <ListGroup.Item bsPrefix="sidebar-top-item">What's on the menu?</ListGroup.Item>
+              <ListGroup.Item bsPrefix="sidebar-top-item">
+                What's on the menu?
+              </ListGroup.Item>
               <ListGroup.Item>Breakfast</ListGroup.Item>
               <ListGroup.Item>Morbi leo risus</ListGroup.Item>
               <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
               <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
             </ListGroup>
+
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Email address"
+              className="mb-2"
+            >
+              <Form.Control type="email" placeholder="name@example.com" />
+            </FloatingLabel>
           </Col>
 
-          <Col md={10} >
-          <Container fluid className="mt-2  cards-container">
-            <Card/>
-            <Card/>
-            <Card/>
-          </Container>
+          <Col md={10}>
+            <Row xs={1} md={2} className="mt-2 p-2 cards-container">
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+            </Row>
           </Col>
         </Row>
       </Container>
+      <Row className="mb-5"></Row>
     </>
   );
 }
