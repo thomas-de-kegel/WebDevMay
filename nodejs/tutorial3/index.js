68d3f5af4eca1651ad8cff89503c8dd55b92752a
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-09-15 11:28:56 +0200

    file system part 1

--- nodejs/tutorial3/index.js
+++ nodejs/tutorial3/index.js
@@ -6,4 +6,6 @@ const fs = require('fs');
 
 fs.readFile('./text.txt','utf8', (err, data) => { //returns as hexadecimal string by default. utf8 argument converts it to normal string
     console.log(data);
+    console.log(data.split(','))
+    data.split(',').forEach(name => console.log(name)); //returns the seperated bits of the array as actual strings
 })
