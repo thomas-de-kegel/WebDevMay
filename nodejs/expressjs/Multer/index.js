020f05a51f16b229afc7baa0acca5814ea57289a
thomas-de-kegel <thomas.dekegel@student.intecbrussel.be>
2022-09-29 09:44:30 +0200

    file uploader works

--- nodejs/expressjs/Multer/index.js
+++ nodejs/expressjs/Multer/index.js
@@ -1,26 +1,34 @@
 //dependencies
-const express = require('express');
-const cors = require('cors');
-const multer = require('multer');
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
 
 //server
 const server = express();
 
 //use middlewares
-server.use(cors())
-server.use(express.json())
+server.use(cors());
+server.use(express.json());
 
-server.get('/', (request,response)=>{
-    response.json({
-        message:"Welcome to restfulAPI"
-    })
-})
+server.get("/", (request, response) => {
+  response.json({
+    message: "Welcome to restfulAPI",
+  });
+});
 
-server.post((request,response)=>{
-    console.log(request.file)
-    response.json({
-        msg:"ok"
-    })
-})
+const storage = multer.diskStorage({
+  destination: (request, file, callback) => callback(null, "./uploads"),
+  filename: (request, file, callback) => callback(null, file.originalname),
+});
+
+const uploader = multer({ storage });
+
+server.post('/',uploader.single('document'),(request, response) => {
+  console.log(request.file.path); // display file location out
+  response.json({
+    msg: "ok",
+  });
+});
 
 //port number for server
+server.listen(4000, () => console.log("Server is running on 4000"))
